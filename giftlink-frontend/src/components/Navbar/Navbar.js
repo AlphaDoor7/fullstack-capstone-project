f9cdfcb88e467d3431a2bd409e1cbbaff934ac9b
AlphaDoor7 <alphadoorr7@gmail.com>
2024-08-12 07:12:43 -0400

    Implement search functionality and style SearchPage.

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -9,13 +9,18 @@ export default function Navbar() {
                 <ul className="navbar-nav">
                     {/* Task 1: Add links to Home and Gifts below*/}
                     <li className="nav-item">
-                    <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}
+                        <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="/app">Gifts</a> {/* Updated Link */}
+                    </li>
+                    {/* Task 2: Add link to SearchPage */}
+                    <li className="nav-item">
+                        <a className="nav-link" href="/app/search">Search</a> {/* Link to SearchPage */}
                     </li>
                 </ul>
             </div>
         </nav>
     );
 }
+
